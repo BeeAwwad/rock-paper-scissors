18e6483b9dd6a1c62e9f4dad0b94b073e62491d8
BeeAwwad <113793663+BeeAwwad@users.noreply.github.com>
2022-11-14 15:24:48 +0100

    Simplified code a bit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function getComputerChoice() {
+    // Computer's choice
     let gameChoices = ["Rock", "Paper", "Scissors"];
-    let computerPick = gameChoices[Math.floor(Math.random() * gameChoices.length)];
-    console.log(computerPick);
+    let computerPick = Math.floor(Math.random() * gameChoices.length);
+    return computerPick;
 }
