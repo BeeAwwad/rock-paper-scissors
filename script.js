20ed0313c90aaf0172b78a8ac00bd5e6db6095d1
BeeAwwad <113793663+BeeAwwad@users.noreply.github.com>
2022-11-15 10:32:34 +0100

    Create a game of papeer beating rock

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function getComputerChoice() {
     // Computer's choice
-    let gameChoices = ["Rock", "Paper", "Scissors"];
+    let gameChoices = ["rock", "paper", "scissors"];
     let computerPick = Math.floor(Math.random() * gameChoices.length);
     return computerPick;
+}
+
+let choicePlayer = "Rock";
+choicePlayer = choicePlayer.toLowerCase();
+let choiceComputer = "Paper";
+choiceComputer = choiceComputer.toLocaleLowerCase();
+
+function game1(a, b){
+
+  if (a === "rock" & b === "paper"){
+    return "You Lose! Paper Beats Rock";
+  }
+  else if(a === "rock" & b === "rock") {
+    return "draw";
+  }
+  else{
+    return "No player";
+  }
 }
