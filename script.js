ec898fc0a41260f8aea713142b8dac9a71d298ce
BeeAwwad <113793663+BeeAwwad@users.noreply.github.com>
2022-11-17 15:38:36 +0100

    Complete the playround function

--- script.js
+++ script.js
@@ -5,22 +5,41 @@ function getComputerChoice() {
     return computerPick;
 }
 
-let choicePlayer = "Rock";
-let choiceComputer = "Paper";
+let playerChoice = "Scissors";
 
-function game1(a, b){
+function playround(a, b){
 
-     a = a.toLocaleLowerCase();
-     b = b.toLocaleLowerCase();
+     a = a.toString().toLowerCase();
+     b = b.toString().toLowerCase();
 
-  if (a === "rock" & b === "paper" || a === "paper" & b === "rock"){
+  if (a === "rock" & b === "paper"){
     
     return "You Lose! Paper Beats Rock";
   }
-  else if(a === "rock" & b === "rock") {
+  else if (a === "paper" & b === "rock"){
+    return "You Win! Paper Beats Rock"
+  }
+  else if(a === "scissors" & b === "rock"){
+    return "You Lose! Rock Beats Scissors"
+  }
+  else if(a === "rock" & b === "scissors"){
+    return "You Win! Rock Beats Scissors"
+  }
+  else if(a === "paper" & b === "scissors"){
+    return "You Lose! Scissors Beats Paper"
+  }
+  else if(a === "scissors" & b === "paper"){
+    return "You Win! Scissors Beats Paper"
+  }
+  else if(a === "rock" & b === "rock" || a === "paper" & b === "paper" || a === "scissors" & b === "scissors") {
     return "draw";
   }
   else{
     return "No player";
   }
+}
+
+
+function game() {
+
 }
